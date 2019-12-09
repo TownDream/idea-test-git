189dd6c24240ee512d15e452968305aee74ee949
TownDream <1070323611@qq.com>
2019-12-09 09:26:59 +0800

    github提交111

--- src/main/java/com/yidu/git/Appla.java
+++ src/main/java/com/yidu/git/Appla.java
@@ -1,5 +1,5 @@
 package com.yidu.git;
 
 public class Appla {
-    private int a = 3;
+    private int a = 31;
 }
--- src/main/java/com/yidu/git/Happy.java
+++ src/main/java/com/yidu/git/Happy.java
@@ -8,7 +8,6 @@ public class Happy {
 		System.out.println("测试分支bbb");
 		System.out.println("晚安——18/8/13");
 		System.out.println("111");
-
         System.out.println("d22d11d");
 	}
 }
